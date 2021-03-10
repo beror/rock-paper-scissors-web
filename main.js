8bba0453e9bbe1b3febac2032b141545b8da72cb
beror <34488024+beror@users.noreply.github.com>
2021-03-10 20:27:49 +0200

    Code improvments and repsonsiveness
    Many changes
    Website is now responsive
    Lots of refactoring and code improvements

--- main.js
+++ main.js
@@ -19,138 +19,116 @@ const shapeOutlineColors = {
 var score;
 if (sessionStorage.getItem("score") !== null) score = sessionStorage.getItem("score");
 else score = 0;
+const winnerHighlightBoxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
 document.getElementById("score").innerHTML = score;
 
-function rockSelectedEvent() {
-  console.log("Rock selected");
+let narrowerThan805pxQuery = window.matchMedia("(max-width: 805px)");
+narrowerThan805pxQuery.addEventListener("change", adaptOutcomeTableToScreen);
+adaptOutcomeTableToScreen();
 
-  let playersShape = shapes.ROCK;
-  let computersShape = Math.floor(Math.random() * 3);
-
-  document.getElementById("shapeChoiceTable").style.display = "none";
-  document.getElementById("outcomeTable").style.visibility = "visible";
-
-  document.getElementById("playersShape").src = "images/icon-rock.svg";
-  document.getElementById("playersShapeBorder").style.borderColor = "hsl(349, 71%, 52%)";
-
-  setTimeout(function() {
-    document.getElementsByClassName("computersShapeCircle")[0].classList.add("doubleBorder");
-    document.getElementsByClassName("computersShapeCircle")[0].classList.remove("computersShapeCircle");
-    document.getElementById("computersShape").src = "images/icon-" + Object.keys(shapes).find(shape => shapes[shape] === computersShape).toLowerCase() + ".svg";
-    document.getElementById("computersShape_div").style.borderColor = shapeOutlineColors[Object.keys(shapes).find(shape => shapes[shape] === computersShape)];
-    document.getElementById("computersShape").style.display = "";
-  }, 500);
-
-
-  if (computersShape === playersShape) {
-    console.log("Stalemate. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "STALEMATE";
-  } else if (computersShape === shapeThatIsBeatenBy.ROCK) {
-    console.log("Player wins. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "YOU WIN";
-    setTimeout(function() {
-      document.getElementById("playersShapeBorder").style.boxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
-    }, 1500);
-    document.getElementById("score").innerHTML = ++score;
-  } else {
-    console.log("Computer wins. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "YOU LOSE";
-    setTimeout(function() {
-      document.getElementById("computersShape_div").style.boxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
-    }, 1500);
-    document.getElementById("score").innerHTML = --score;
+function shapeSelectedEvent(shape) {
+  if(!isValidShapeName(shape)) {
+    console.log("Invalid shape name");
+    return;
   }
+  console.log(shape + " selected");
 
-  document.getElementById("outcomeAnnouncmentCell").classList.add("scaleIn");
-}
-
-function paperSelectedEvent() {
-  console.log("Paper selected");
-
-  let playersShape = shapes.PAPER;
+  let playersShape = shapes[shape];
   let computersShape = Math.floor(Math.random() * 3);
+  let computersShapeStr = Object.keys(shapes).find(shape_ => shapes[shape_] === computersShape);
 
-  document.getElementById("shapeChoiceTable").style.display = "none";
-  document.getElementById("outcomeTable").style.visibility = "visible";
-
-  document.getElementById("playersShape").src = "images/icon-paper.svg";
-  document.getElementById("playersShapeBorder").style.borderColor = "hsl(230, 89%, 62%)";
-
-  setTimeout(function() {
-    document.getElementsByClassName("computersShapeCircle")[0].classList.add("doubleBorder");
-    document.getElementsByClassName("computersShapeCircle")[0].classList.remove("computersShapeCircle");
-    document.getElementById("computersShape").src = "images/icon-" + Object.keys(shapes).find(shape => shapes[shape] === computersShape).toLowerCase() + ".svg";
-    document.getElementById("computersShape_div").style.borderColor = shapeOutlineColors[Object.keys(shapes).find(shape => shapes[shape] === computersShape)];
-    document.getElementById("computersShape").style.display = "";
-  }, 500);
-
-
-  if (computersShape === playersShape) {
-    console.log("Stalemate. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "STALEMATE";
-  } else if (computersShape === shapeThatIsBeatenBy.ROCK) {
-    console.log("Player wins. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "YOU WIN";
-    setTimeout(function() {
-      document.getElementById("playersShapeBorder").style.boxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
-    }, 1500);
-    document.getElementById("score").innerHTML = ++score;
-  } else {
-    console.log("Computer wins. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "YOU LOSE";
-    setTimeout(function() {
-      document.getElementById("computersShape_div").style.boxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
-    }, 1500);
-    document.getElementById("score").innerHTML = --score;
-  }
-
-  document.getElementById("outcomeAnnouncmentCell").classList.add("scaleIn");
-}
-
-function scissorsSelectedEvent() {
-  console.log("Scissors selected");
-
-  let playersShape = shapes.SCISSORS;
-  let computersShape = Math.floor(Math.random() * 3);
-
-  document.getElementById("shapeChoiceTable").style.display = "none";
-  document.getElementById("outcomeTable").style.visibility = "visible";
-
-  document.getElementById("playersShape").src = "images/icon-scissors.svg";
-  document.getElementById("playersShapeBorder").style.borderColor = "hsl(39, 89%, 49%)";
-
-  setTimeout(function() {
-    document.getElementsByClassName("computersShapeCircle")[0].classList.add("doubleBorder");
-    document.getElementsByClassName("computersShapeCircle")[0].classList.remove("computersShapeCircle");
-    document.getElementById("computersShape").src = "images/icon-" + Object.keys(shapes).find(shape => shapes[shape] === computersShape).toLowerCase() + ".svg";
-    document.getElementById("computersShape_div").style.borderColor = shapeOutlineColors[Object.keys(shapes).find(shape => shapes[shape] === computersShape)];
-    document.getElementById("computersShape").style.display = "";
-  }, 500);
-
-
-  if (computersShape === playersShape) {
-    console.log("Stalemate. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "STALEMATE";
-  } else if (computersShape === shapeThatIsBeatenBy.ROCK) {
-    console.log("Player wins. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "YOU WIN";
-    setTimeout(function() {
-      document.getElementById("playersShapeBorder").style.boxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
-    }, 1500);
-    document.getElementById("score").innerHTML = ++score;
-  } else {
-    console.log("Computer wins. Computer chose " + computersShape + ", the player chose " + playersShape);
-    document.getElementById("outcomeAnnouncment").innerHTML = "YOU LOSE";
-    setTimeout(function() {
-      document.getElementById("computersShape_div").style.boxShadow = "0px 8px rgba(189, 194, 214, 0.7) inset, 0 0 0 20px rgba(255, 255, 255, 0.05), 0 0 0 70px rgba(255, 255, 255, 0.03), 0 0 0 120px rgba(255, 255, 255, 0.01)";
-    }, 1500);
-    document.getElementById("score").innerHTML = --score;
-  }
-
-  document.getElementById("outcomeAnnouncmentCell").classList.add("scaleIn");
+  processOutcome(playersShape, computersShape);
 }
 
 function restartGame() {
   sessionStorage.setItem("score", score);
   window.location.reload(false);
+}
+
+function isValidShapeName(shape) {
+  return Object.keys(shapes).indexOf(shape) !== -1 ? true : false;
+}
+
+function appendChildren(parent, children) {
+  let appendedChildren = [];
+  for(let i = 0; i < children.length; ++i) {
+    appendedChildren.push(parent.appendChild(children[i]));
+  }
+
+  return appendedChildren.length === 0 ? null : appendedChildren;
+}
+
+function removeChildren(parent) {
+  let removedChildren = [];
+  while(parent.lastChild) {
+    removedChildren.push(parent.removeChild(parent.lastChild));
+  }
+
+  return removedChildren.length === 0 ? null : removedChildren;
+}
+
+function moveOutcomeAnnounementBelowShapes() {
+  outcomeTable = document.getElementById("outcomeTable");
+  if(outcomeTable.rows.length === 3) return;
+  let newRow = outcomeTable.insertRow(-1);
+  newRow.appendChild(document.getElementById("outcomeAnnouncmentCell")).colSpan = "2";
+  document.getElementById("blankCellBetweenOutcomeChoiceLabels").style.display = "none";
+  outcomeTable.rows[1].cells[0].style.paddingRight = "20px";
+  outcomeTable.rows[1].cells[outcomeTable.rows[1].cells.length - 1].style.paddingLeft = "20px";
+}
+
+function insertOutcomeAnnounementBetweenShapes() {
+  outcomeTable = document.getElementById("outcomeTable");
+  if(outcomeTable.rows.length === 2) return;
+  outcomeTable.rows[1].cells[0].insertAdjacentElement("afterend", document.getElementById("outcomeAnnouncmentCell")).colSpan = "1";
+  outcomeTable.deleteRow(-1);
+  document.getElementById("blankCellBetweenOutcomeChoiceLabels").style.display = "";
+}
+
+function adaptOutcomeTableToScreen() {
+  if(narrowerThan805pxQuery.matches) {
+    moveOutcomeAnnounementBelowShapes();
+  } else {
+    insertOutcomeAnnounementBetweenShapes();
+  }
+}
+
+function processOutcome(playersShape, computersShape) {
+  let playersShapeStr = Object.keys(shapes).find(shape_ => shapes[shape_] === playersShape);
+  let computersShapeStr = Object.keys(shapes).find(shape_ => shapes[shape_] === computersShape);
+
+  document.getElementById("shapeChoiceTable").style.display = "none";
+  document.getElementById("outcomeTable").style.visibility = "visible";
+
+  document.getElementById("playersShape").src = "images/icon-" + playersShapeStr.toLowerCase() + ".svg";
+  document.getElementById("playersShapeBorder").style.borderColor = shapeOutlineColors[playersShapeStr];
+
+  setTimeout(function() {
+    document.getElementById("computersShape_div").classList.add("shapeDoubleBorder");
+    document.getElementById("computersShape_div").classList.remove("shapePlaceholder");
+    document.getElementById("computersShape").src = "images/icon-" + computersShapeStr.toLowerCase() + ".svg";
+    document.getElementById("computersShape_div").style.borderColor = shapeOutlineColors[computersShapeStr];
+    document.getElementById("computersShape").style.display = "";
+  }, 500);
+
+  if (computersShape === playersShape) {
+    console.log("Stalemate. Computer chose " + computersShape + ", the player chose " + playersShape);
+    document.getElementById("outcomeAnnouncment").innerHTML = "STALEMATE";
+  } else if (computersShape === shapeThatIsBeatenBy[playersShapeStr]) {
+    console.log("Player wins. Computer chose " + computersShape + ", the player chose " + playersShape);
+    document.getElementById("outcomeAnnouncment").innerHTML = "YOU WIN";
+    setTimeout(function() {
+      document.getElementById("playersShapeBorder").style.boxShadow = winnerHighlightBoxShadow;
+    }, 1500);
+    document.getElementById("score").innerHTML = ++score;
+  } else {
+    console.log("Computer wins. Computer chose " + computersShape + ", the player chose " + playersShape);
+    document.getElementById("outcomeAnnouncment").innerHTML = "YOU LOSE";
+    setTimeout(function() {
+      document.getElementById("computersShape_div").style.boxShadow = winnerHighlightBoxShadow;
+    }, 1500);
+    document.getElementById("score").innerHTML = --score;
+  }
+
+  document.getElementById("outcomeAnnouncmentCell").classList.add("scaleIn");
 }
